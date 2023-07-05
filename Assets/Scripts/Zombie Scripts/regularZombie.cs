6b97330608d599a4be14f0002a4866bddb4d44e2
Jazmine Chargualaf <jazmine.chargualaf@gmail.com>
2023-07-04 17:39:59 -0700

    Regular zombie navigation
    regular zombie roams and follows player

--- Assets/Scripts/Zombie Scripts/regularZombie.cs
+++ Assets/Scripts/Zombie Scripts/regularZombie.cs
@@ -8,22 +8,109 @@ public class regularZombie : MonoBehaviour, IDamage
     [Header("Components")]
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform headPos;
 
     [Header("Regular Zombie Stats")]
     [SerializeField] int hp = 5;
     [SerializeField] int speed = 5;
     [SerializeField] int damage;
 
+    [Header("Regular Zombie Navigation")]
+    [Range(10, 360)][SerializeField] int viewAngle;
+    [Range(1, 8)][SerializeField] int playerFaceSpeed;
+    [SerializeField] int roamTimer;
+    [SerializeField] int roamDist;
 
+    bool playerInRange;
+    Vector3 playerDir;
+    float angleToPlayer;
+    float stoppingDistanceOrig;
+    Vector3 startingPos;
+    bool destinationChosen;
 
     void Start()
     {
-        
+        stoppingDistanceOrig = agent.stoppingDistance;
+        startingPos = transform.position;
     }
 
     void Update()
     {
-        
+        if (playerInRange && !canSeePlayer())
+        {
+            StartCoroutine(roam());
+        }
+        else if (agent.destination != gameManager.instance.player.transform.position)
+            StartCoroutine(roam());
+    }
+
+    IEnumerator roam()
+    {
+        if (agent.remainingDistance < 0.05f && !destinationChosen)
+        {
+            destinationChosen = true;
+            agent.stoppingDistance = 0;
+            yield return new WaitForSeconds(roamTimer);
+
+            Vector3 randomPos = Random.insideUnitSphere * roamDist;
+            randomPos += startingPos;
+
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomPos, out hit, roamDist, 1);
+            agent.SetDestination(hit.position);
+
+            destinationChosen = false;
+        }
+    }
+
+    void facePlayer()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, 0, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * playerFaceSpeed);
+    }
+
+    bool canSeePlayer()
+    {
+        agent.stoppingDistance = stoppingDistanceOrig;
+        playerDir = gameManager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(new Vector3(playerDir.x, 0, playerDir.z), transform.forward);
+
+        Debug.DrawRay(headPos.position, playerDir);
+        Debug.Log(angleToPlayer);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer < viewAngle)
+            {
+                agent.SetDestination(gameManager.instance.player.transform.position);
+
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    facePlayer();
+                }
+
+                return true;
+            }
+        }
+        agent.stoppingDistance = 0;
+        return false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
     }
 
     public void TakeDamage(int amount)
