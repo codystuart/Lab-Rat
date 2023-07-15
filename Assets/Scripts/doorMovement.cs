17293c80d3638757a95900b35d8ac789aad6ec38
Jazmine Chargualaf <jazmine.chargualaf@gmail.com>
2023-07-15 14:31:50 -0700

    Updated Interactable Door Prefab

---
+++ Assets/Scripts/doorMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class doorMovement : MonoBehaviour
+{
+    [SerializeField] bool playerInRange;
+    [SerializeField] bool doorIsOpen;
+    [SerializeField] GameObject doorInteractText;
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown("e"))
+        {
+            Debug.Log("Door opens");
+            doorIsOpen = true;
+            hideText();
+            transform.localRotation = Quaternion.Euler(0, 0, 90); 
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player")) 
+        {
+            playerInRange = true;
+            if (!doorIsOpen)
+            {
+                showText();
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            hideText();
+        }
+    }
+
+    void showText()
+    {
+         doorInteractText.SetActive(true);
+    }
+    void hideText()
+    {
+        doorInteractText.SetActive(false);
+    }
+}
