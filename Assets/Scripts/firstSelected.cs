b196a62addc94fae901142342f257926408017ea
Jazmine Chargualaf <jazmine.chargualaf@gmail.com>
2023-08-13 12:04:27 -0700

    Revert "Merge branch 'main' of https://github.com/CodeWithJazmine/Lab-Rat"
    This reverts commit ab0d0dfd32e5992dca11159cb298af9ac7894e7a, reversing
    changes made to 71aa055f455429c7368fe5ee0a445f4cbd4f0157.

--- Assets/Scripts/firstSelected.cs
+++ Assets/Scripts/firstSelected.cs
@@ -3,22 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class firstSelected : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class firstSelected : MonoBehaviour
 {
     //sets the first selected button of each menu
     [SerializeField] GameObject first;
 
     void Start()
-    {
-        EventSystem.current.SetSelectedGameObject(first);
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        EventSystem.current.SetSelectedGameObject(null);
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
     {
         EventSystem.current.SetSelectedGameObject(first);
     }
