bb20dfb5eb6feddbe5ff9cc2d5d1fb4550c643d0
Jazmine Chargualaf <jazmine.chargualaf@gmail.com>
2023-08-25 22:23:16 -0700

    UI Prefab Update

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-// using UnityEditor.Search;
+using UnityEditor.Search;
 
 public class gameManager : MonoBehaviour
 {
